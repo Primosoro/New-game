43af215fc0ca32950188d3135e5f5eb1dbfda91c
Primo Tiongco <ptiongco95@yahoo.com>
2018-04-10 17:31:05 -0700

    yes

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+//using UnityStandardAssets.Characters.FirstPerson;
+
+//public class DialogueManager : MonoBehaviour {
+
+	//public GameObject diaLogueBox;
+	//public Text diaLogueText;
+
+	//public UnityChanControlScriptWithRgidBody player;
+
+	//public bool diaLogueActive;
+
+	//public bool talkingtoSaphire;
+
+	//public string[] diaLogueLines;
+
+	//public internal currentLine; 
+
+	//void Start() 
+	//{
+	//	diaLogueBox.SetActive (false);
+	//}
+
+	//void Update () {
+		//if (diaLogueActive && Input.GetKeyDown(KeyCode.Return)) {
+
+		//	PlayerPrefs.movem
+	 
